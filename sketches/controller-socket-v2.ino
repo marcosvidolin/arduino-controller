1dbf5403f9a1034753666fcac4c4ba06ba3e8cf8
marcosvidolin <marcosvidolin@gmail.com>
2013-05-15 23:21:30 -0300

    Refactoring

--- sketches/controller-socket-v2.ino
+++ sketches/controller-socket-v2.ino
@@ -9,6 +9,10 @@
 #include <Ethernet.h>
 #include <Servo.h>
 
+EthernetServer server = EthernetServer(80);
+Servo myservo;
+
+
 const char BUTTON_1 = 'A';
 //const char BUTTON_2 = 'B';
 //const char BUTTON_3 = 'C';
@@ -30,38 +34,98 @@ const char STICK_1_AXIS_Y = 'R';
 const char STICK_2_AXIS_X = 'S';
 const char STICK_2_AXIS_Y = 'T';
 
-EthernetServer server = EthernetServer(80);
-byte ip[] = {192, 168, 1, 199};
-byte gateway[] = {192, 168, 1, 1}; 
-byte subnet[] = {255, 255, 255, 0};
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-
-Servo myservo;
 
 unsigned long lastTime = 0;
 boolean isRunning = false;
 
+
 /**
- * Setup method.
- *
+ * Stops all servors.
  */
-void setup() {
+void stopMotors() {
+  myservo.write(90);
+  isRunning = false;
+}
+
+/**
+ * Setups servors.
+ */
+void setupMotors() {
+  myservo.attach(9);
+  stopMotors();
+}
+
+/**
+ * Setups the ethernet.
+ */
+void setupEthernet() {
   // start the Ethernet connection and the server:
+  byte ip[] = {192, 168, 1, 199};
+  byte gateway[] = {192, 168, 1, 1}; 
+  byte subnet[] = {255, 255, 255, 0};
+  byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
-  
-  // attaches the servo on pin 9 to the servo object 
-  myservo.attach(9);
-  // stops the servo
-  myservo.write(90);
+}
 
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+/**
+ * Sends the command to Arduino.
+ *
+ * @param button
+ *      - the button pressed
+ * @param value
+ *      - the value of the button pressed
+ */
+void sendCommandToArduino(char button, int value) {
+  switch(button) {
+    case BUTTON_1:
+      break;
+    case STICK_2_AXIS_Y:
+      myservo.write(value);
+      isRunning = true;
+      lastTime = millis();     
+      //delay(1);
+      break;
+    case STICK_2_AXIS_X:
+      //lastTime = millis();
+      break;
+  }
+}
+
+/**
+ * Convert a String to float value.
+ * 
+ * @param text
+ *      - the String
+ *
+ * @return float value
+ */
+float stringToFloat(String text) {
+  char carray[text.length() + 1];
+  text.toCharArray(carray, sizeof(carray));
+  return atof(carray);
+}
+
+/**
+ * Setup method.
+ */
+void setup() {
+  setupMotors();
+  setupEthernet();
+}
+
+/**
+ * Check if the motor is some time without receiving commands to run.
+ * Returns true if no commands.
+ *
+ * @return boolean
+ */
+boolean isMotorsActivityTimeout() {
+  return (millis() - lastTime) > 250;
 }
 
 /**
  * Loop method.
- *
  */
 void loop() {
   // listen for incoming clients
@@ -87,59 +151,19 @@ void loop() {
         } else if (characters > 2 && characters < 6) {
           value += c;
           if (characters == 5) {
-            //Serial.print(button);Serial.println(value.toInt());
             sendCommandToArduino(button, value.toInt());
             characters = 0;
             value = "";
-            lastTime = millis();
           }
         } 
       }
       
       // stops the servo when there is no activity
-      //Serial.print(millis()); Serial.print("  >  "); Serial.println(lastTime);
-      if (isRunning && (millis() - lastTime) > 250) {
-        myservo.write(90);
-        Serial.println("Parou");
-        isRunning = false;
+      if (isRunning && isMotorsActivityTimeout()) {
+        stopMotors();
       }
       
     }
     client.stop();
   }
-}
-
-
-/**
- * Sends the command to Arduino.
- *
- */
-void sendCommandToArduino(char button, int value) {
-  switch(button) {
-    case BUTTON_1:
-      break;
-    case STICK_2_AXIS_Y:
-      Serial.println(value);
-      myservo.write(value);
-      isRunning = true;     
-      //delay(1);
-      break;
-    case STICK_2_AXIS_X:
-      break;
-  }
-}
-
-/**
- * Convert a String to float value.
- * 
- * @param text
- *      - the String
- *
- * @return float value
- *
- */
-float stringToFloat(String text) {
-  char carray[text.length() + 1];
-  text.toCharArray(carray, sizeof(carray));
-  return atof(carray);
 }
